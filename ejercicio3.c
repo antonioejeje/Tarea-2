d7b1aeff22e6c19f6ae2e862296e4987265cb6c0
antonioejeje <joseantoniohernaj@unam.edu>
2026-05-03 12:00:40 -0600

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+  int main()
+  {
+float p,     dol;
+printf ("Ingresa la cantidad de doloares que deseas convetir\n");
+scanf ("%f", &dol);
+p = dol * 11.96;
+  printf ("la cantidad en pesos es: %f", p);
+  return 0;
+}
