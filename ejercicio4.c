e4c4193d0e893460ee78efde513de48244bd1f90
antonioejeje <joseantoniohernaj@unam.edu>
2026-05-03 12:06:54 -0600

    Create ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+  int main()
+{
+  float dinero, total, i=11;
+printf ("catidad a invertir: \n");
+scanf ("%f", &dinero);
+total = (dinero * 0.11) + dinero; 
+printf ("la suma total de dinero es: %f", total);
+return 0;
+}
