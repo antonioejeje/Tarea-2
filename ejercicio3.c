d38c5cdc7663ffb70936d050b2909347d6e5c0b5
antonioejeje <joseantoniohernaj@unam.edu>
2026-05-15 13:34:34 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -2,7 +2,7 @@
   int main()
   {
 float p,     dol;
-printf ("Ingresa la cantidad de doloares que deseas convetir\n");
+printf ("Ingresa la cantidad de dolares que deseas convertir\n");
 scanf ("%f", &dol);
 p = dol * 11.96;
   printf ("la cantidad en pesos es: %f", p);
